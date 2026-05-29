37ce3a618df6d102e4f3875e9088900f2afda89e
Natchathra <nnatchathra68@gmail.com>
2026-05-29 18:22:08 +0530

    Added employee entity and repository

---
+++ src/main/java/com/natchathra/employee_management/entity/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.natchathra.employee_management.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository <Employee , Long >{
+
+}

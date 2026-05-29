3f8b305409bd5707d33f3c76ed585c616d6f2226
Natchathra <nnatchathra68@gmail.com>
2026-05-29 18:36:37 +0530

    Added employee service and REST APIs

--- src/main/java/com/natchathra/employee_management/repository/EmployeeRepository.java
+++ src/main/java/com/natchathra/employee_management/repository/EmployeeRepository.java
@@ -1,8 +1,8 @@
-package com.natchathra.employee_management.entity;
+package com.natchathra.employee_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import com.natchathra.employee_management.entity.Employee;
 @Repository
 public interface EmployeeRepository extends JpaRepository <Employee , Long >{
 
---
+++ src/main/java/com/natchathra/employee_management/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.natchathra.employee_management.service;
+
+import com.natchathra.employee_management.entity.Employee;
+import com.natchathra.employee_management.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
